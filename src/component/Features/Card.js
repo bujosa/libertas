4807b84843a0614d188a882f5d0fa686baa9f9dc
bujosa <davidbujosa@gmail.com>
2023-08-15 23:24:32 -0400

    feat: add hover change image color

--- src/component/Features/Card.js
+++ src/component/Features/Card.js
@@ -1,15 +1,23 @@
-import React from "react"
+import Reac, { useState } from 'react';
 
 const Card = (props) => {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
     <>
-      <div className='box btn_shadow'>
-        <img src={props.image} alt='' />
+      <div
+        className={`box btn_shadow ${isHovered ? 'hovered' : ''}`}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}>
+        <img
+          src={isHovered ? props.image + '&color=FFFFFF' : props.image}
+          alt=""
+        />
         <h2>{props.title}</h2>
         <p>{props.desc}</p>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
